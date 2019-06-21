a92d7e181cb557a96d92336309bb4a55685b2ab6
AdamVen <33135394+AdamVen@users.noreply.github.com>
2019-06-20 20:02:29 -0700

    Added files for support of SRF02 ultrasonic sensor
    Added required device class, interface, testing fake, simulated and unit / integration test files for eventual support of the SRF02 ultrasonic sensor

---
+++ LineBotIntegrationTests/UltrasonicSensorIntegrationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LineBotIntegrationTests
+{
+    [TestClass]
+    public class UltrasonicSensorIntegrationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
